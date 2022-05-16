d14001f55d1f170912b6caad2a2ba4894da6c0a9
Sanchaksh Kaul <43877199+Sanchaksh@users.noreply.github.com>
2022-05-16 16:40:52 +0530

    Final Commit

--- src/main/java/com/oracle/userservice/repository/UserRepository.java
+++ src/main/java/com/oracle/userservice/repository/UserRepository.java
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
 package com.oracle.userservice.repository;
 
 import com.oracle.userservice.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByUserId(String id);
-}
-
-=======
-package com.oracle.userservice.repository;
-
-import com.oracle.userservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
     User findByUserId(String id);
 }
 
->>>>>>> 0123469b063a7d675e6b6f59eb1374fefd8816aa
+
+
--- src/main/java/com/oracle/userservice/service/UserService.java
+++ src/main/java/com/oracle/userservice/service/UserService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.oracle.userservice.service;
 
 import com.oracle.userservice.model.User;
@@ -34,40 +33,4 @@ public class UserService {
         return repository.save(user1);
     }
 }
-=======
-package com.oracle.userservice.service;
 
-import com.oracle.userservice.model.User;
-import com.oracle.userservice.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class UserService {
-    @Autowired
-    private UserRepository repository;
-
-    public User saveUser(User user) { return repository.save(user); }
-
-    public Optional<User> findbyId(Integer id) { return repository.findById(id); }
-
-    public List<User> getUsers() {
-        return repository.findAll();
-    }
-
-    public String delete(Integer id) { repository.deleteById(id); return "Deleted!!";};
-
-    public User update(Integer id,User user) {
-        User user1=repository.findById(id).orElseThrow();
-        user1.setName(user.getName());
-        user1.setPhoneNo(user.getPhoneNo());
-        user1.setMail(user.getMail());
-        user1.setState(user.getState());
-        user1.setAge(user.getAge());
-        return repository.save(user1);
-    }
-}
->>>>>>> 0123469b063a7d675e6b6f59eb1374fefd8816aa
